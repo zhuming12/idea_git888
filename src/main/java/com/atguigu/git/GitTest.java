f1a55811beb305639c315b3a76ee0697386f0bec
朱明 <482354907@qq.com>
2022-06-02 09:38:22 +0800

    第二次更新

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("新建文件");
         System.out.println("第一次更新");
+        System.out.println("第二次更新");
     }
 }
